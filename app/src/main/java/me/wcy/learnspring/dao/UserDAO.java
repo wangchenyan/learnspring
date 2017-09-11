7611d1fb78ac958c25b21f0f2f32bc05a832442e
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-09-11 20:27:55 +0800

    添加EhCache

--- app/src/main/java/me/wcy/learnspring/dao/UserDAO.java
+++ app/src/main/java/me/wcy/learnspring/dao/UserDAO.java
@@ -1,15 +1,22 @@
 package me.wcy.learnspring.dao;
 
 import me.wcy.learnspring.po.User;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 
 /**
  * Created by hzwangchenyan on 2017/9/7.
  */
 public interface UserDAO {
+    String CACHE_PREFIX = "c.user";
+
+    @CacheEvict(value = CACHE_PREFIX, key = "#user.user_name")
     int insert(User user) throws DataAccessException;
 
+    @CacheEvict(value = CACHE_PREFIX, key = "#user.user_name")
     int update(User user) throws DataAccessException;
 
+    @Cacheable(value = CACHE_PREFIX, key = "#user_name")
     User query(String user_name) throws DataAccessException;
 }
