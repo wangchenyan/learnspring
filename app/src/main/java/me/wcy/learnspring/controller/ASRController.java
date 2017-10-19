7792975fe072b226fe18071151cbe09e4fc4e54c
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-10-19 11:27:24 +0800

    ASR

--- app/src/main/java/me/wcy/learnspring/controller/ASRController.java
+++ app/src/main/java/me/wcy/learnspring/controller/ASRController.java
@@ -3,7 +3,6 @@ package me.wcy.learnspring.controller;
 import com.alibaba.fastjson.JSON;
 import me.wcy.learnspring.common.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ import java.util.List;
 @RestController
 public class ASRController {
 
-    @RequestMapping(value = "/api/asr", method = RequestMethod.POST)
+    @RequestMapping("/api/asr")
     public Response callback(
             @RequestParam("ret_code") Integer ret_code,
             @RequestParam("ret_msg") String ret_msg,
