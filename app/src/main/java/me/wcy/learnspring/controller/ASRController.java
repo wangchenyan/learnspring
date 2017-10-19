4ebb6d1e20b8b2310309338ddbf93b666107bfe7
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-10-19 11:35:15 +0800

    ASR

--- app/src/main/java/me/wcy/learnspring/controller/ASRController.java
+++ app/src/main/java/me/wcy/learnspring/controller/ASRController.java
@@ -3,11 +3,9 @@ package me.wcy.learnspring.controller;
 import com.alibaba.fastjson.JSON;
 import me.wcy.learnspring.common.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Created by hzwangchenyan on 2017/10/19.
@@ -17,10 +15,10 @@ public class ASRController {
 
     @RequestMapping("/api/asr")
     public Response callback(
-            @RequestParam("ret_code") Integer ret_code,
-            @RequestParam("ret_msg") String ret_msg,
-            @RequestParam("sn") String sn,
-            @RequestParam("result") List<String> result,
+//            @RequestParam("ret_code") Integer ret_code,
+//            @RequestParam("ret_msg") String ret_msg,
+//            @RequestParam("sn") String sn,
+//            @RequestParam("result") List<String> result,
             HttpServletRequest request
     ) {
         System.out.println(JSON.toJSONString(request.getParameterMap()));
