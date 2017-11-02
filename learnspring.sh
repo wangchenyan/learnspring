bca8825eb35d763a20d2df2b47f937fd3f575ad2
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-11-02 20:25:27 +0800

    添加二维码

--- learnspring.sh
+++ learnspring.sh
@@ -4,32 +4,32 @@
 
 APP_NAME=learnspring
 PROJECT_PATH=/home/wcy/project/learnspring
-JAR_PATH=$PROJECT_PATH/app/target/learnspring.jar
+JAR_PATH=${PROJECT_PATH}/app/target/learnspring.jar
 APP_PATH=/home/wcy/app/learnspring
-DEPLOY_PATH=$APP_PATH/learnspring.jar
-OUTPUT_PATH=$APP_PATH/learnspring.out
+DEPLOY_PATH=${APP_PATH}/learnspring.jar
+OUTPUT_PATH=${APP_PATH}/learnspring.out
 
 
 #Stop
-tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+tpid=`ps -ef|grep ${APP_NAME}|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ ${tpid} ];
 then
     echo '> Stop Process...'
-    kill -15 $tpid
+    kill -15 ${tpid}
 fi
 sleep 5
-tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+tpid=`ps -ef|grep ${APP_NAME}|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ ${tpid} ];
 then
     echo '> Kill Process!'
-    kill -9 $tpid
+    kill -9 ${tpid}
 else
     echo '> Stop Success!'
 fi
 
 
 #Package
-cd $PROJECT_PATH
+cd ${PROJECT_PATH}
 
 echo '> Update Code...'
 git pull
@@ -40,16 +40,16 @@ mvn clean
 echo '> Package...'
 mvn package
 
-if [ -f $JAR_PATH ];
+if [ -f ${JAR_PATH} ];
 then
     echo '> Package Success!'
-    cp -rf $JAR_PATH $DEPLOY_PATH
+    cp -rf ${JAR_PATH} ${DEPLOY_PATH}
     echo '> Copy Success!'
 
 
     #Start
-    cd $APP_PATH
-    nohup java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar $DEPLOY_PATH > $OUTPUT_PATH 2>&1 &
+    cd ${APP_PATH}
+    nohup java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ${DEPLOY_PATH} > ${OUTPUT_PATH} 2>&1 &
     echo '> Start Success!'
 else
     echo '> Package Failed!'
