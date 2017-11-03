55a17602ef1369de8b2c205d025388a323ab883f
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-11-03 11:06:17 +0800

    部署脚本

--- learnspring.sh
+++ learnspring.sh
@@ -31,7 +31,7 @@ if [ -f ${JAR_PATH} ];then
 
     #Stop
     tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ ${tpid} ]];then
+    if [ ${tpid} ];then
         echo '> Stop Process...'
         kill -15 $tpid
     fi
@@ -39,7 +39,7 @@ if [ -f ${JAR_PATH} ];then
     sleep 5
 
     tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ ${tpid} ]];then
+    if [ ${tpid} ];then
         echo '> Kill Process!'
         kill -9 $tpid
     else
