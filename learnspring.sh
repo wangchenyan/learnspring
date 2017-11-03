8182c4857a9394d9ff47b6fe410398be45eba1a8
hzwangchenyan <hzwangchenyan@corp.netease.com>
2017-11-03 11:25:01 +0800

    部署脚本

--- learnspring.sh
+++ learnspring.sh
@@ -11,24 +11,12 @@ OUTPUT_PATH=${APP_PATH}/learnspring.out
 
 
 #Stop
-tpid=`ps -ef|grep learnspring|grep -v grep|grep -v kill|awk '{print $2}'`
-echo 'comond is ' `ps -ef|grep learnspring|grep -v grep|grep -v kill|awk '{print $2}'`
-echo 'tpid is '$tpid
-
-if [ ${tpid} ];then
-    echo '> Stop Process...'
-    kill -15 $tpid
-fi
-
-sleep 5
-
-tpid=`ps -ef|grep learnspring|grep -v grep|grep -v kill|awk '{print $2}'`
-if [ ${tpid} ];then
-    echo '> Kill Process!'
-    kill -9 $tpid
-else
-    echo '> Stop Success!'
-fi
+PROCESS=`ps -ef|grep ${APP_NAME}|grep -v grep|grep -v PPID|awk '{ print $2}'`
+for i in ${PROCESS}
+do
+    echo "> Kill the ${APP_NAME} process [ ${i} ]"
+    kill -9 ${i}
+done
 
 
 #Package
