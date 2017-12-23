370e232afe834ce76970492debca82ff2b9eb9cd
wcy <919247701@qq.com>
2017-12-23 16:53:47 +0800

    关闭Kafka

--- learnspring.sh
+++ learnspring.sh
@@ -10,7 +10,7 @@ DEPLOY_PATH=${APP_PATH}/learnspring.jar
 OUTPUT_PATH=${APP_PATH}/learnspring.out
 
 #Stop
-PID=`ps -ef|grep ${PROCESS_NAME}|grep -v grep|grep -v PPID|awk '{ print $2}'`
+PID=`ps -ef|grep ${PROCESS_NAME}|grep -v grep|grep -v PPID|awk '{print $2}'`
 if [ ${PID} ];then
     echo '> Kill Process!'
     kill -9 ${PID}
@@ -35,7 +35,7 @@ if [ -f ${JAR_PATH} ];then
 
     #Start
     cd ${APP_PATH}
-    nohup java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ${DEPLOY_PATH} > ${OUTPUT_PATH} 2>&1 &
+    java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ${DEPLOY_PATH} > ${OUTPUT_PATH} 2>&1 &
     echo '> Start Success!'
 else
     echo '> Package Failed!'
