702069bcf405d70afe164a328adef5e47b843276
wcy <919247701@qq.com>
2018-01-14 17:13:07 +0800

    update

--- app/src/main/java/me/wcy/spring/app/controller/XiaocaoController.java
+++ app/src/main/java/me/wcy/spring/app/controller/XiaocaoController.java
@@ -1,5 +1,6 @@
 package me.wcy.spring.app.controller;
 
+import me.wcy.spring.app.common.HttpClient;
 import me.wcy.spring.app.common.Response;
 import me.wcy.spring.app.remote.consumer.PrinterConsumerService;
 import me.wcy.spring.app.remote.consumer.XiaocaoConsumerService;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by hzwangchenyan on 2017/9/11.
@@ -25,10 +29,24 @@ public class XiaocaoController {
 
     @RequestMapping(value = "/xiaocao", method = RequestMethod.GET)
     public Response xiaocao() {
-        if (xiaocaoConsumerService == null) {
-            return new Response("null");
+//        if (xiaocaoConsumerService == null) {
+//            return new Response("null");
+//        }
+//        return new Response(xiaocaoConsumerService.getXiaocao());
+
+        String url = "http://get.xunfs.com/app/listapp.php";
+        Map<String, String> form = new HashMap<>();
+        form.put("a", "get");
+        form.put("system", "android");
+        form.put("v", "1.4");
+
+        String result;
+        try {
+            result = HttpClient.postFormData(url, null, form);
+        } catch (Exception e) {
+            result = "fail";
         }
-        return new Response(xiaocaoConsumerService.getXiaocao());
+        return new Response(result);
     }
 
     @RequestMapping(value = "print", method = RequestMethod.GET)
